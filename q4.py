c3d85d639db4495f374c8d05d9dc719659d3558c
starry-skies <mmazin2028@kehillahstudent.org>
2025-03-24 08:55:21 -0700

    done

--- q4.py
+++ q4.py
@@ -4,7 +4,8 @@ import json
 # Check if the file "data.json" exists.
 if os.path.isfile("data.json"):
     # If it does, read "data.json" into the variable `data`
-
+    with open("data.json", "r") as f:
+        data = json.load(f)
 
 else:
     # If it doesn't, make an empty dictionary called data
@@ -18,8 +19,7 @@ recommendation = input("What book/movie/podcast/etc. would you recommend? ")
 data[name] = recommendation
 
 # Write the `data` variable to the file "data.json"
+with open("data.json", "w") as f:
+    json.dump(data, f)
 
-####
-#### YOUR CODE HERE 
-####
 
